eee3677d5281385d3719700a4890db75f28e95c9
jasperge <jaspervannieuwenhuizen@gmail.com>
2014-06-16 22:21:09 +0200

    Fix for addon_dir if more then 2 addon script paths

--- util.py
+++ util.py
@@ -43,11 +43,9 @@ thread_count = multiprocessing.cpu_count()
 EnableDebug = True
 
 # Addon directory.
-addon_paths = bpy.utils.script_paths( "addons")
-if 'blenderseed' in os.listdir( addon_paths[0]):
-    addon_dir = os.path.join( addon_paths[0], 'blenderseed')
-else:
-    addon_dir = os.path.join( addon_paths[1], 'blenderseed')
+for addon_path in bpy.utils.script_paths("addons"):
+    if "blenderseed" in os.listdir(addon_path):
+        addon_dir = os.path.join(addon_path, "blenderseed")
 
 version = str(bl_info['version'][1]) + "." + str(bl_info['version'][2])
 
