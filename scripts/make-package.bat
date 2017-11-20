d1647ca2cb34f382de5e5289d1ee71f6befb9c88
Franz Beaune <franz@appleseedhq.net>
2017-11-20 09:01:32 +0100

    Exclude README.md file from package

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -3,7 +3,7 @@
 set zipcmd=%~dp0\tools\7z\7z.exe
 
 pushd %~dp0\..\..
-%zipcmd% a -r -x!scripts -xr!.* -x!__pycache__ blenderseed-x.x.x-yyyy.zip blenderseed
+%zipcmd% a -r -x!scripts -xr!.* -x!__pycache__ -x!README.md blenderseed-x.x.x-yyyy.zip blenderseed
 popd
 
 move ..\..\blenderseed-x.x.x-yyyy.zip .
