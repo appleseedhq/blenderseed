2917f380127603bf130aaa10c27a0f72df9e1f94
Franz Beaune <franz@appleseedhq.net>
2017-12-08 14:24:50 +0100

    Tweak packaging script

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -3,9 +3,9 @@
 set zipcmd=%~dp0\tools\7z\7z.exe
 
 pushd %~dp0\..\..
-%zipcmd% a -r -x!scripts -xr!.* -x!__pycache__ -x!README.md blenderseed-x.x.x-yyyy.zip blenderseed
+%zipcmd% a -r -x!scripts -xr!.* -x!__pycache__ -x!README.md blenderseed-x.x.x.zip blenderseed
 popd
 
-move ..\..\blenderseed-x.x.x-yyyy.zip .
+move ..\..\blenderseed-x.x.x.zip .
 
 pause
