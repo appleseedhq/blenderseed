6dd281e3391c59d86eadd45f58be42f4bc5d1bfc
François Beaune <franz@appleseedhq.net>
2018-04-24 13:37:29 +0200

    Fix double registration of compatible engines

--- ui/scene.py
+++ ui/scene.py
@@ -32,7 +32,6 @@ import bpy
 def register():
     bpy.types.SCENE_PT_scene.COMPAT_ENGINES.add('APPLESEED_RENDER')
     bpy.types.SCENE_PT_color_management.COMPAT_ENGINES.add('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_custom_props.COMPAT_ENGINES.add('APPLESEED_RENDER')
     bpy.types.SCENE_PT_audio.COMPAT_ENGINES.add('APPLESEED_RENDER')
     bpy.types.SCENE_PT_unit.COMPAT_ENGINES.add('APPLESEED_RENDER')
     bpy.types.SCENE_PT_keying_sets.COMPAT_ENGINES.add('APPLESEED_RENDER')
@@ -45,15 +44,14 @@ def register():
 
 
 def unregister():
-    bpy.types.SCENE_PT_scene.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_color_management.COMPAT_ENGINES.remove('APPLESEED_RENDER')
     bpy.types.SCENE_PT_custom_props.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_audio.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_unit.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_keying_sets.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_keying_set_paths.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_physics.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_rigid_body_world.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_rigid_body_cache.COMPAT_ENGINES.remove('APPLESEED_RENDER')
     bpy.types.SCENE_PT_rigid_body_field_weights.COMPAT_ENGINES.remove('APPLESEED_RENDER')
-    bpy.types.SCENE_PT_custom_props.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_rigid_body_cache.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_rigid_body_world.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_physics.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_keying_set_paths.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_keying_sets.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_unit.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_audio.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_color_management.COMPAT_ENGINES.remove('APPLESEED_RENDER')
+    bpy.types.SCENE_PT_scene.COMPAT_ENGINES.remove('APPLESEED_RENDER')
