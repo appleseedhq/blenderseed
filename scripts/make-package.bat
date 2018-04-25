1343e77f448eaf783d1fc7ae86442ca0ee62f3e4
Jonathan Dent <jdent02@hotmail.com>
2018-04-25 06:53:25 -0500

    Tweak packaging script for spaces in filenames

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set zipcmd=%~dp0\tools\7z\7z.exe
+set zipcmd="%~dp0\tools\7z\7z.exe"
 
 pushd %~dp0\..\..
 %zipcmd% a -r -x!scripts -xr!.* -x!__pycache__ -x!docs -x!README.md blenderseed-x.x.x.zip blenderseed
