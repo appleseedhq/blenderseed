7b5f0489102a4c80e8a46ffd055e1bc8316e40a0
Luke Kliber <30292603+vallmoy@users.noreply.github.com>
2018-04-29 15:54:26 +0200

    Update world.py

--- properties/world.py
+++ properties/world.py
@@ -50,7 +50,7 @@ class AppleseedSkySettings(bpy.types.PropertyGroup):
     env_type = bpy.props.EnumProperty(name="Environment Type",
                                       items=[("constant", "Constant", "Use constant color for sky"),
                                              ("gradient", "Gradient", "Use sky color gradient"),
-                                             ("latlong_map", "HDRI Environment", "Use hdri map texture"),
+                                             ("latlong_map", "HDRI Environment", "Use HDRI map texture"),
                                              ("mirrorball_map", "Mirror Ball", "Use mirror ball texture"),
                                              ("sunsky", "Physical Sky", ""),
                                              ("constant_hemisphere", "Per-Hemisphere Constant", "Use constant color per hemisphere")],
