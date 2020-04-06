4827b2b3598ae567acd8421412b5cd8d77fbc126
Jonathan Dent <jdent02@hotmail.com>
2020-04-06 12:45:37 -0500

    Fix for #504

--- properties/nodes.py
+++ properties/nodes.py
@@ -102,8 +102,6 @@ class AppleseedOSLNode(bpy.types.Node):
                     image = getattr(self, (x['name']))
                     layout.prop(image, "filepath", text="Filepath")
                 else:
-                    if hasattr(self, "%s_use_node" % x['label']):
-                        socket_number += 1
                     if getattr(self, param_section):
                         label_text = x['label']
                         if x['type'] in ('color', 'vector', 'float[2]'):
@@ -117,8 +115,11 @@ class AppleseedOSLNode(bpy.types.Node):
                             col.prop(self, x['name'], text=label_text)
                             col = split.column(align=True)
                             col.prop(self, "%s_use_node" % x['label'], text="", toggle=True, icon='NODETREE')
+                            socket_number += 1
                         else:
                             layout.prop(self, x['name'], text=label_text)
+                    elif hasattr(self, "%s_use_node" % x['label']):
+                        socket_number += 1
             else:
                 socket_number += 1
 
