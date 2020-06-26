6efb8a87b26aeb30f0e6c8224d9612838c3fa6c9
jdent02 <jdent02@hotmail.com>
2020-06-26 12:25:23 -0500

    Update for feedback

--- utils/osl_utils.py
+++ utils/osl_utils.py
@@ -305,7 +305,11 @@ def generate_node(node, node_class):
 
         elif param['type'] == "float":
 
-            kwargs = {'name': param['name'], 'description': helper, 'default': float(default)} if 'default' in keys else {'name': param['name'], 'description': helper}
+            kwargs = {'name': param['name'], 'description': helper}
+            
+            if 'default' in keys:
+                kwargs['default'] = float(default)
+            
             if minimum is not None:
                 kwargs['min'] = float(minimum)
             if maximum is not None:
